977b080c46d6d6aa6c56dbedb0979be25f563b50
Vishakha Verma <vishakhaverma236@gmail.com>
2026-01-12 01:09:07 +0530

    Initial Android project commit

---
+++ app/src/main/java/com/kth/liveabcdkiddo/M.java
@@ -0,0 +1,50 @@
+package com.kth.liveabcdkiddo;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class M extends AppCompatActivity {
+    Button btn_next;
+    ImageView iv_play;
+    MediaPlayer m;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_m);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        btn_next = findViewById(R.id.btn_next);
+        iv_play = findViewById(R.id.iv_play);
+        playMusic();
+        btn_next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(M.this,N.class));
+            }
+        });
+        iv_play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+playMusic();
+            }
+        });
+    }
+    private void playMusic() {
+        m = MediaPlayer.create(getApplicationContext(),R.raw.m);
+        m.start();
+    }
+}
